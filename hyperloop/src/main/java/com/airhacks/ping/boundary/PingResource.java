ba1c8f5ea799640f1603b89903917a0ae8affc05
Adam Bien <abien@adam-bien.com>
2018-02-07 06:10:51 +0100

    Thanks for attending the session @CGI. See you at airhacks.com, airhacks.io or at least at airhacks.tv

--- hyperloop/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ hyperloop/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -1,18 +1,40 @@
 package com.airhacks.ping.boundary;
 
+import com.airhacks.messaging.control.RemoteDrill;
+import java.util.concurrent.TimeUnit;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 /**
  *
  * @author airhacks.com
  */
+@Stateless
 @Path("ping")
 public class PingResource {
 
+    @Inject
+    String message;
+
+    @Inject
+    RemoteDrill drill;
+
     @GET
-    public String ping() {
-        return "Enjoy Java EE 8 !" + System.currentTimeMillis();
+    public void ping(@Suspended AsyncResponse response) {
+        response.setTimeout(1, TimeUnit.SECONDS);
+        drill.message().
+                thenApply(this::decorateWithMessage).
+                thenAccept(response::resume);
     }
+
+    String decorateWithMessage(String input) {
+        return input + this.message;
+    }
+
+    //return this.drill.message() + this.message + System.currentTimeMillis();
 
 }
